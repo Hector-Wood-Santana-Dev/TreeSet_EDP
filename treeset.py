64c20a7cfe5be7cd443875e5601b82914b9fa6d5
magna99 <153322164+magna99@users.noreply.github.com>
2025-04-27 18:51:26 +0100

    Update treeset.py
    metodos del arbol

--- treeset.py
+++ treeset.py
@@ -1,1 +1,63 @@
 # Implementación del árbol rojo-negro en python
+class treeset:
+
+    def __init__(self):
+        self.lenght = 0
+
+    def __len__(self):
+        return self.lenght
+
+    def add(self, e):
+        return True
+
+    def adAll(self, collection):
+        return True
+
+    def  ceiling(self):
+        return True
+
+    def clear(self):
+        return True
+
+    def clone(self):
+        return True
+
+    def contains(self):
+        return True
+
+    def descendingIterator(self):
+        yield
+
+    def firts(self):
+        return value
+
+    def floor(self,e):
+        return vakye
+
+    def higher(self,e):
+        return value
+
+    def isEmpty(self):
+        return True
+
+    def iterator(self):
+        yield
+
+    def last(self):
+        return value
+
+    def lower(self):
+        return value
+
+    def pollFirts(self):
+        return value
+
+    def pollLast(self):
+        return value
+
+    def remove(self, o):
+        return True
+
+    def size(self):
+        return size
+  
